c01c80f8f8e2424412f062855371de6f8d20d28b
Tawab Shaikh <tawabshaikh14444@gmail.com>
2026-03-26 16:00:13 +0530

    endmailportchange

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -4,10 +4,12 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // App Password
+    pass: process.env.EMAIL_PASS,
   },
 });
 
